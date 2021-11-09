17dba55237500574b355000bfa6e4a9b6edb8fba
Vinicius Scampini <viniciuscscampini@gmail.com>
2021-11-09 12:12:30 -0300

    Adicionado a API e a consulta basica de autenticação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,AppRoutingModule
+    BrowserModule,
+    AppRoutingModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
